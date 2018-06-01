d2e8dce36e358370a4b6f68faed6a9cb9bb4ca71
LapoFrati <fratilapo@gmail.com>
2018-06-01 19:35:24 +0100

    Delete unnecessary files

--- test/brainSpec.js
+++
@@ -1,7 +0,0 @@
-describe('A test suite', function() {
-    it('A test', function() {
-        var tensor = new Brain(2, 2);
-        var numTensors = tf.memory().numTensors;
-        expect(numTensors).to.equal(2);
-    });
-});
